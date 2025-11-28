ae7a74afd70e5d8d8125cab6fd6946e3387f201a
jexpop <jexpouoc@gmail.com>
2025-11-29 00:21:18 +0100

    RNC

--- P2 RNC/cnn.py
+++ P2 RNC/cnn.py
@@ -22,23 +22,33 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Dropout
 
 # Inicializar la CNN
 classifier = Sequential()
 
 # Paso 2 - Convolucion
 classifier.add(Convolution2D(32, 3, 3, 
-               input_shape = (64, 64, 3),
+               input_shape = (128, 128, 3),
                activation = "relu"))
 
 # Paso 2 - Max Pooling
 classifier.add(MaxPooling2D(pool_size = (2,2)))
 
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Adding a third convolutional layer
+classifier.add(Convolution2D(64, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
 # Paso 3 - Flattering
 classifier.add(Flatten())
 
 # Paso 4  - Full Connection
-classifier.add(Dense(units = 128, activation = "relu"))
+classifier.add(Dense(units = 64, activation = "relu"))
+classifier.add(Dropout(rate = 0.5))
 classifier.add(Dense(units = 1, activation = "sigmoid"))
 
 # Compilar la CNN
@@ -60,21 +70,42 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_dataset = train_datagen.flow_from_directory('C:/Users/JEXPO/Downloads/dataset/training_set',
-                                                    target_size=(64, 64),
+                                                    target_size=(128, 128),
                                                     batch_size=32,
                                                     class_mode='binary')
 
 testing_dataset = test_datagen.flow_from_directory('C:/Users/JEXPO/Downloads/dataset/test_set',
-                                                        target_size=(64, 64),
+                                                        target_size=(128, 128),
                                                         batch_size=32,
                                                         class_mode='binary')
 
+
 classifier.fit(training_dataset,
                         steps_per_epoch=8000,
-                        epochs=25,
+                        epochs=50,
                         validation_data=testing_dataset,
                         validation_steps=2000)
 
+
+# Parte 3 - Reto
+import numpy as np
+from keras.preprocessing import image
+
+single_imgen = image.load_img('dataset/single_prediction/IMG-20251126-WA0004.jpeg',
+                     target_size=(128, 128))
+
+single_imgen = image.img_to_array(single_imgen)
+
+single_imgen = np.expand_dims(single_imgen, axis = 0)
+
+result = classifier.predict(single_imgen)
+
+#training_dataset.class_indices
+
+if result[0][0] == 1:
+    prediction = 'perro'
+else:
+    prediction = 'gato'
 
 
 
